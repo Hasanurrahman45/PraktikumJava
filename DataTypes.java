8f89168f7fcee1ddc7c1ee7af1347eed40d44c02
hasanurrahman <rahman.hasanur45@gmail.com>
2021-06-11 02:10:26 +0800

    output dengan sout

--- DataTypes.java
+++ DataTypes.java
@@ -9,5 +9,14 @@ public class DataTypes{
         double ipk = 3.89764512;
         char nilaAbjad = 'A';
         boolean tampan = true;
+
+	System.out.println("Nama Depan : " + namaDepan);
+        System.out.println("Nama Tengah : " + namaTengah);
+        System.out.println("Nama Belakang : " + namaBelakang);
+        System.out.println("Usia : " + usia);
+        System.out.println("Target Kuliah : " + targetTahunKuliah + " tahun");
+        System.out.println("IPK : " + ipk );
+        System.out.println("Nilai PBO : " + nilaAbjad);
+        System.err.println("Tampan : " + tampan);
  }
 }
