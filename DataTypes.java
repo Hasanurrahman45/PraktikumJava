1bded94664d3f0b5e7e312480329e5c5c2a8f790
hasanurrahman <rahman.hasanur45@gmail.com>
2021-06-11 02:04:56 +0800

    deklarasi variabel dan tipe data

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes{
+    public static void main(String[] args) {
+
+        String namaDepan = "Hasanur";
+        String namaTengah = "rahman";
+        String namaBelakang = "19630404";
+        int usia = 21;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaAbjad = 'A';
+        boolean tampan = true;
+ }
+}
