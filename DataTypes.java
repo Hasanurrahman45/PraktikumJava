e1f73ca4dc87791836bb866d5f31e9a6283b49bd
hasanurrahman <rahman.hasanur45@gmail.com>
2021-06-11 03:15:28 +0800

    input dengan input dialog

--- DataTypes.java
+++ DataTypes.java
@@ -1,26 +1,43 @@
 import javax.swing.JOptionPane;
+import java.util.*;
 
 public class DataTypes{
-    public static void main(String[] args) {
+ public static void main(String[] args){
+  String namaDepan = "Hasanur";
+  String namaBelakang = "Rahman";
+  int usia = 21;
+  int lamaKuliah = 4;
+  double ipk =3.89;
+  char nilai = 'A';
+  boolean tampan = true;
 
-        String namaDepan = "Hasanur";
-        String namaTengah = "rahman";
-        String namaBelakang = "19630404";
-        int usia = 21;
-        int targetTahunKuliah = 4;
-        double ipk = 3.89764512;
-        char nilaAbjad = 'A';
-        boolean tampan = true;
+  Scanner input = new Scanner(System.in);
 
-	System.out.println("Nama Depan : " + namaDepan);
-        System.out.println("Nama Tengah : " + namaTengah);
-        System.out.println("Nama Belakang : " + namaBelakang);
-        System.out.println("Usia : " + usia);
-        System.out.println("Target Kuliah : " + targetTahunKuliah + " tahun");
-        System.out.println("IPK : " + ipk );
-        System.out.println("Nilai PBO : " + nilaAbjad);
-        System.err.println("Tampan : " + tampan);
-	
-	JOptionPane.showMessageDialog(null, "Hai, " + namaDepan + namaTengah +" "+ namaBelakang);
+  System.out.println("==== INPUT ====");
+  System.out.print("Input nama depan : ");
+  namaDepan = input.nextLine();
+  System.out.print("Input usia : ");
+  usia = input.nextInt();
+  System.out.print("Input IPK : ");
+  ipk = input.nextDouble();
+  System.out.print("");
+  System.out.print("Input Nilai Abjad : ");
+  nilai = input.next().charAt(0);
+  System.out.print("Tampan? : ");
+  tampan = input.nextBoolean();
+
+  System.out.println("==== OUTPUT ===="); 
+  System.out.println("Nama Depan : " + namaDepan); 
+  System.out.println("Nama Belakang : " + namaBelakang);
+  System.out.println("Usia : " + usia);
+  System.out.println("Target Kuliah : " + lamaKuliah + " Tahun");
+  System.out.println("IPK : " + ipk);
+  System.out.println("Nilai : " + nilai);
+  System.out.println("Tampan : " + tampan);
+
+  int usiaLulus = usia + lamaKuliah;
+  System.out.println("Usia Lulus : " + usiaLulus + " Tahun");
+
+  JOptionPane.showMessageDialog(null, "Hai, " + namaDepan + " " + namaBelakang);
  }
 }
